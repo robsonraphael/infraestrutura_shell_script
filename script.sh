ff7348b0787b1825bc115694cbff38cbe6ee5985
Robson Raphael <robsonraphael2001@gmail.com>
2022-10-01 22:20:41 -0300

    Fix adduser

--- script.sh
+++ script.sh
@@ -11,6 +11,34 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
+echo "Criando usuários com permissões ADM: "
+echo "Carlos..."
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt carlos123) -G GRP_ADM
+echo "Maria..."
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt maria123) -G GRP_ADM
+echo "João..."
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt joao123) -G GRP_ADM
+
+
+echo "Criando usuários com permissões VEN: "
+echo "Debora..."
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt debora123) -G GRP_VEN
+echo "Sebastiana..."
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt sebastiana123) -G GRP_VEN
+echo "Roberto..."
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt roberto123) -G GRP_VEN
+
+
+
+echo "Criando usuários com permissões SEC: "
+echo "Josefina..."
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt josefina123) -G GRP_SEC
+echo "Amanda..."
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt amanda123) -G GRP_SEC
+echo "Rogerio..."
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt rogerio123) -G GRP_SEC
+
+
 echo "Especificando permissões dos diretórios..."
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -21,21 +49,5 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Quantos usuários deseja criar ?"
-read indice
-
-for (i = 1, i <= indice, i++) do 
-    echo "Nome do usuário: "
-    read usuario
-    
-    echo "Senha do usuário: "
-    read password
-
-    grupos = ('GRP_ADM', 'GRP_VEN', 'GRP_SEC')
-    echo "Grupo disponiveis: $grupos"
-    echo "Grupo do usuário: "
-    read user_grp
-    useradd $usuario -m -s /bin/bash -p $(openssl passwd -crypt $password) -G $user_grp
-done
 
 echo "FIM...."
